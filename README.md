018ac501e4d6aba9007fa675be52800f19f0db0b
Sébastien Guillotin <sebgusa@gmail.com>
2025-03-21 08:23:24 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 9-2Collections_GuillotinFischer
+ 9.2 Collections Exercise by Sebastien Guillotin and Autumn Fischer
---
+++ sketch.js
@@ -0,0 +1,24 @@
+/**
+ * Sebastien Guillotin, m/d/25
+ * IGME-102: Assignment Name
+ * Description and interaction instructions
+ */
+
+"use strict"; //catch some common coding errors
+
+/* Global variables */
+
+/**
+ * setup :
+ */
+function setup() {
+   createCanvas(400, 400);
+
+}
+
+/**
+ * draw :
+ */
+function draw() {
+
+}
