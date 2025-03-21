6f3027564f43dd8fff8e5442794ae2b7aa949848
Sébastien Guillotin <sebgusa@gmail.com>
2025-03-21 08:38:04 -0400

    added header
    for all files, and sketch-specific one

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
 /**
- * Sebastien Guillotin, m/d/25
- * IGME-102: Assignment Name
- * Description and interaction instructions
+ * Sebastien Guillotin, Autumn Fischer 3/21/25
+ * IGME-102: 9-2 Exercise: Collection Palooza
+ * Testing every method/function/property option we
+ * have learned for each data type.
+ * 
+ * Every which way of Arrays, Objects, Maps,
+ * Sets, Iterators, DIY Stacks, and DIY Queues.
  */
 
 "use strict"; //catch some common coding errors
